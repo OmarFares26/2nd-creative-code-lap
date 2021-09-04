f9b03657e3d09f22be68f34af0ee5415e882cd45
Omar Fares <90091619+OmarFares26@users.noreply.github.com>
2021-09-04 13:50:20 +0200

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    dbPassword: 'K[tv3XV!',
-    access_token_secret: '2fbab4a232c50322765da15ceebbe6991372d657ff3e3161c2f6cf42d3c1a974b77455691ad0f90d73911983c658f963a9fddf7414ba3afa0992deb3bb022680'
-};
